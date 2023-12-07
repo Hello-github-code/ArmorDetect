f471382af0b92cc7fc1e9c1ddd0a9341787280d5
Hello-github-code <153065481+Hello-github-code@users.noreply.github.com>
2023-12-07 17:17:29 +0800

    Update 001.c
    练习

--- 001.c
+++ 001.c
@@ -1,5 +1,9 @@
 #include <stdio.h>
+#include <math.h>
+
 int main() {
-  printf("Hello!");
+  printf("Hello!\n");
+  for (int i = 0; i < 10; i++) {
+    printf("%4d %4d\n", i, pow(i, 2));
   return 0;
 }
